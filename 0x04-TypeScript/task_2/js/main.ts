93ee1e1a41d5e674f935550b142286b55388fd93
Nana133 <odosaint@gmail.com>
2025-07-13 01:38:42 +0000

    Add Director and Teacher interfaces with createEmployee function in task_2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -80,4 +80,55 @@ function teachClass(todayClass: Subjects): string {
 }
 
 console.log(teachClass("Math"));
-console.log(teachClass("History"));
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Example usage
+console.log(createEmployee(200));   // Teacher instance
+console.log(createEmployee(1000));  // Director instance
+console.log(createEmployee('$500')); // Director instanceconsole.log(teachClass("History"));
